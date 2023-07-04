2750fb316bd5951f59efb065f6f48af98ae4b73d
Joris Pevcas <joris@pevcas.eu>
2023-07-05 01:58:48 +0300

    lcd gpio without HAL and branchless

--- Core/Src/lcd.c
+++ Core/Src/lcd.c
@@ -1,7 +1,7 @@
 #include "lcd.h"
 #include "main.h"
 
-/* FONT FUNCTIONS BEGIN */
+/* FONT BEGIN */
 
 #define FONT_H 8
 #define FONT_W 5
@@ -24,70 +24,93 @@ const char *get_char_data(uint8_t c) {
 	return ret;
 }
 
-/* FONT FUNCTIONS END */
+/* FONT END */
 
+#define GET_GPIO_PORT(pin) (pin ## _GPIO_Port)
+#define GET_GPIO_PIN_NUM(pin) (pin ## _Pin)
+
+#define GET_GPIO_MASK(pin) GET_GPIO_PIN_NUM(pin)
+#define GET_GPIO_MASK_INV(pin) (0xffff ^ GET_GPIO_MASK(pin))
+
+#define SET_H(pin) GET_GPIO_PORT(pin)->ODR |= GET_GPIO_MASK(pin)
+#define SET_L(pin) GET_GPIO_PORT(pin)->ODR &= GET_GPIO_MASK_INV(pin)
 #define SET_PIN(pin, state) HAL_GPIO_WritePin(pin ## _GPIO_Port, pin ## _Pin, state)
+#define GET_BSRR_MASK(pin, data) (GET_GPIO_MASK(LCD_D ## pin) << (16 * !(((data) >> pin) & 1)))
 
 void lcd_write(uint8_t data) {
-	SET_PIN(LCD_WR, 0);
+	SET_L(LCD_WR);
 
-	SET_PIN(LCD_D0, (data >> 0) & 1);
-	SET_PIN(LCD_D1, (data >> 1) & 1);
-	SET_PIN(LCD_D2, (data >> 2) & 1);
-	SET_PIN(LCD_D3, (data >> 3) & 1);
-	SET_PIN(LCD_D4, (data >> 4) & 1);
-	SET_PIN(LCD_D5, (data >> 5) & 1);
-	SET_PIN(LCD_D6, (data >> 6) & 1);
-	SET_PIN(LCD_D7, (data >> 7) & 1);
+	GPIOF->BSRR |= \
+		GET_BSRR_MASK(0, data) | \
+		GET_BSRR_MASK(2, data) | \
+		GET_BSRR_MASK(4, data) | \
+		GET_BSRR_MASK(7, data);
 
-	SET_PIN(LCD_WR, 1);
+	GPIOE->BSRR |= \
+		GET_BSRR_MASK(3, data) | \
+		GET_BSRR_MASK(5, data) | \
+		GET_BSRR_MASK(6, data);
+
+	GPIOD->BSRR |= \
+		GET_BSRR_MASK(1, data);
+
+//	SET_PIN(LCD_D0, (data >> 0) & 1);
+//	SET_PIN(LCD_D1, (data >> 1) & 1);
+//	SET_PIN(LCD_D2, (data >> 2) & 1);
+//	SET_PIN(LCD_D3, (data >> 3) & 1);
+//	SET_PIN(LCD_D4, (data >> 4) & 1);
+//	SET_PIN(LCD_D5, (data >> 5) & 1);
+//	SET_PIN(LCD_D6, (data >> 6) & 1);
+//	SET_PIN(LCD_D7, (data >> 7) & 1);
+
+	SET_H(LCD_WR);
 }
 
 static inline void lcd_command_write(uint8_t command) {
-	SET_PIN(LCD_RS, 0);
+	SET_L(LCD_RS);
 	lcd_write(command);
 }
 
 static inline void lcd_data_write(uint8_t data) {
-	SET_PIN(LCD_RS, 1);
+	SET_H(LCD_RS);
 	lcd_write(data);
 }
 
 void lcd_set_address(int16_t y1, int16_t y2, int16_t x1, int16_t x2) {
-	 lcd_command_write(0x2a);
-	 lcd_data_write(y1 >> 8);
-	 lcd_data_write(y1);
-	 lcd_data_write(y2 >> 8);
-	 lcd_data_write(y2);
+	lcd_command_write(0x2a);
+	lcd_data_write(y1 >> 8);
+	lcd_data_write(y1);
+	lcd_data_write(y2 >> 8);
+	lcd_data_write(y2);
 
-	 lcd_command_write(0x2b);
-	 lcd_data_write(x1 >> 8);
-	 lcd_data_write(x1);
-	 lcd_data_write(x2 >> 8);
-	 lcd_data_write(x2);
+	lcd_command_write(0x2b);
+	lcd_data_write(x1 >> 8);
+	lcd_data_write(x1);
+	lcd_data_write(x2 >> 8);
+	lcd_data_write(x2);
 }
 
 void lcd_set_pixel(int16_t x, int16_t y, int16_t color) {
-	SET_PIN(LCD_CS, 0);
+	SET_L(LCD_CS);
 	lcd_set_address(y, y+1, x, x+1);
 	lcd_command_write(0x2c);
 	lcd_data_write(color >> 8);
 	lcd_data_write(color);
-	SET_PIN(LCD_CS, 1);
+	SET_H(LCD_CS);
 }
 
 void lcd_init(void) {
-	SET_PIN(LCD_RST, 1);
+	SET_H(LCD_RST);
 	HAL_Delay(20);
-	SET_PIN(LCD_RST, 0);
+	SET_L(LCD_RST);
 	HAL_Delay(20);
-	SET_PIN(LCD_RST, 1);
+	SET_H(LCD_RST);
 	HAL_Delay(20);
 
-	SET_PIN(LCD_CS, 1);
-	SET_PIN(LCD_WR, 1);
-	SET_PIN(LCD_RD, 1);
-	SET_PIN(LCD_CS, 0);
+	SET_H(LCD_CS);
+	SET_H(LCD_WR);
+	SET_H(LCD_RD);
+	SET_L(LCD_CS);
 
 	// VCOM1
 	lcd_command_write(0xc5);
@@ -120,10 +143,6 @@ void lcd_init(void) {
 		}
 
 		const char *ch = get_char_data(curr_ch - ' ');
-		for(int i = 0; i < CHAR_SIZE; i++) {
-			printf("%x ", ch[i]);
-		}
-		putc('\n', stdout);
 		int8_t shift = 7;
 		for(int i = 0; i < FONT_W * FONT_H; i++) {
 			if(*ch & (1 << shift)) {
@@ -139,5 +158,5 @@ void lcd_init(void) {
 			}
 		}
 	}
-	SET_PIN(LCD_CS, 1);
+	SET_H(LCD_CS);
 }
